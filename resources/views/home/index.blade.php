166c3184cabf939efd61e16f1f1893ec0bdcbec5
AswadS15 <aswadsupu5@gmail.com>
2024-03-04 00:05:45 +0800

    ok

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-ee1baeca.css') }}" data-navigate-track="reload">
-    <script type="module" src="{{ asset('build/assets/app-b1941ff8.js') }}" data-navigate-track="reload"></script> --}}
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-ee1baeca.css') }}" data-navigate-track="reload">
+    <script type="module" src="{{ asset('build/assets/app-b1941ff8.js ') }}" data-navigate-track="reload"></script>
+    {{-- @vite(['resources/css/app.css','resources/js/app.js']) --}}
     <link rel="stylesheet" type="text/css" href="{{ asset('https://unpkg.com/trix@2.0.8/dist/trix.css') }}">
     <script type="text/javascript" src="{{ asset('https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js') }}"></script>
     <title>Layanan Buku Tamu BPKHTK XV Gorontalo</title>
